f4c037326267adaf65a0f27bce1b590dc00f78b7
G.M <47867339+GMLJ@users.noreply.github.com>
2021-10-22 11:53:42 +0200

    Creation aboutMe component

--- src/components/Particle.js
+++ src/components/Particle.js
@@ -2,15 +2,6 @@ import React from "react";
 import Particles from "react-particles-js";
 
 const Particle = () => {
-  const particlesInit = (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   return (
     <Particles
       params={{
