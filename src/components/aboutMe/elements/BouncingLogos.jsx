e81d2882b9ddf2ffc8a5aeeb75e4efac24998d89
G.M <47867339+GMLJ@users.noreply.github.com>
2021-10-22 13:11:02 +0200

    bouncingLogos adjustments

--- src/components/aboutMe/elements/BouncingLogos.jsx
+++ src/components/aboutMe/elements/BouncingLogos.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import { SiBlueprint, SiMongodb } from "react-icons/si";
 import { motion } from "framer-motion";
 
 const BouncingBalls = () => {
   return (
-    <div>
+    <div className="bouncingLogos">
       <motion.img
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png"
         alt="HTML logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -28,9 +27,7 @@ const BouncingBalls = () => {
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png"
         alt="CSS logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -48,9 +45,7 @@ const BouncingBalls = () => {
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sass/sass.png"
         alt="SASS logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -68,9 +63,7 @@ const BouncingBalls = () => {
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png"
         alt="JavaScript logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -88,9 +81,7 @@ const BouncingBalls = () => {
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png"
         alt="REACT logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -108,9 +99,7 @@ const BouncingBalls = () => {
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png"
         alt="NodeJS logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -124,15 +113,10 @@ const BouncingBalls = () => {
           y: ["10%", "-10%"],
         }}
       ></motion.img>
-      <motion.img
-        src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png"
-        alt="MongoDB logo"
+
+      <motion.div
         style={{
-          position: "relative",
-          top: "1rem",
-          marginRight: "2rem",
-          width: "5rem",
-          height: "auto",
+          margin: "0 0.4rem 0 0.8rem",
         }}
         transition={{
           y: {
@@ -143,16 +127,17 @@ const BouncingBalls = () => {
           },
         }}
         animate={{
-          y: ["10%", "-10%"],
+          y: ["35%", "15%"],
         }}
-      ></motion.img>
+      >
+        <SiMongodb size="2.5rem" color="#4AA44E" />
+      </motion.div>
+
       <motion.img
         src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/visual-studio-code/visual-studio-code.png"
         alt="VSCode logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
@@ -170,9 +155,7 @@ const BouncingBalls = () => {
         src="https://camo.githubusercontent.com/926050f1c795978fee869e0b07f678b4a2a4dc6360fd85ca2ea882a5c6394e18/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f3237302f3237303739382e706e67"
         alt="Github logo"
         style={{
-          marginRight: "2rem",
-          width: "2.5rem",
-          height: "auto",
+          scale: 0.5,
         }}
         transition={{
           y: {
