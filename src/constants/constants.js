5c6aaa723f8b5c2abb6accd7657ebc0e246fb7a9
G.M <47867339+GMLJ@users.noreply.github.com>
2021-10-26 19:30:22 +0200

    projects added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -32,30 +32,22 @@ export const projects = [
     id: 0,
   },
   {
+    title: "Blog",
+    description:
+      "Blog construit en MERN Stack. Avec fonctionnalités d'enregistrement de profil, login, création et post de nouveaux articles etc...",
+    image: "/images/blog.png",
+    tags: ["MongoDB", "Express", "React", "Node js"],
+    source: "https://github.com/GMLJ/Blog-MERN-Stack",
+    id: 1,
+  },
+  {
     title: "Carte Choroplèthe",
     description:
       'Carte Choroplèthe réalisée avec D3.js pour la certification "Data Visualisation" de FreeCodeCamp.',
     image: "/images/choropleth.png",
     tags: ["html", "css", "js", "D3"],
     source: "https://codepen.io/GMLJ/pen/VwWWyVN",
-    id: 1,
-  },
-  {
-    title: "Critiques JV",
-    description:
-      "Application basique afin de découvrir le développement mobile sur React Native.",
-    image: "/images/reviews.png",
-    tags: ["react native", "expo"],
-    source: "https://github.com/GMLJ/Reviews-App-React-Native",
     id: 2,
-  },
-  {
-    title: "Shifumi",
-    description: 'Jeu de "pierre, papier, ciseaux" codé en Javascript.',
-    image: "/images/shifumi.png",
-    tags: ["html", "css", "js"],
-    source: "https://github.com/GMLJ/RockPaperScissors",
-    id: 3,
   },
   {
     title: "Podomoro",
@@ -64,6 +56,50 @@ export const projects = [
     image: "/images/podomoro.png",
     tags: ["html", "css", "js"],
     source: "https://codepen.io/GMLJ/pen/vYmyxYv",
+    id: 3,
+  },
+  {
+    title: "Heat Map",
+    description:
+      'Carte Thermique réalisée avec D3.js pour la certification "Data Visualisation" de FreeCodeCamp',
+    image: "/images/heatmap.png",
+    tags: ["html", "css", "js", "D3"],
+    source: "https://codepen.io/GMLJ/full/dyROgGR",
     id: 4,
+  },
+  {
+    title: "Calculatrice",
+    description:
+      'Calculatrice en Javascript réalisée pour la certification "Front End Development Librairies" de FreeCodeCamp',
+    image: "/images/calc.png",
+    tags: ["html", "css", "js"],
+    source: "https://codepen.io/GMLJ/pen/ExmVpmO",
+    id: 5,
+  },
+  {
+    title: "Bar Chart",
+    description:
+      'Diagramme réalisée avec D3.js pour la certification "Data Visualisation" de FreeCodeCamp',
+    image: "/images/barchart.png",
+    tags: ["html", "css", "js", "D3"],
+    source: "https://codepen.io/GMLJ/details/YzQzvYr",
+    id: 6,
+  },
+  {
+    title: "Critiques JV",
+    description:
+      "Application basique afin de découvrir le développement mobile sur React Native.",
+    image: "/images/reviews.png",
+    tags: ["react native", "expo"],
+    source: "https://github.com/GMLJ/Reviews-App-React-Native",
+    id: 7,
+  },
+  {
+    title: "Shifumi",
+    description: 'Jeu de "pierre, papier, ciseaux" codé en Javascript.',
+    image: "/images/shifumi.png",
+    tags: ["html", "css", "js"],
+    source: "https://github.com/GMLJ/RockPaperScissors",
+    id: 8,
   },
 ];
