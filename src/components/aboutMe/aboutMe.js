1500f3ac2d406bf4b0fb5151ea26dc99845f02f7
G.M <47867339+GMLJ@users.noreply.github.com>
2022-01-14 20:52:33 +0300

    bug fix

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion, useTransform, useViewportScroll } from "framer-motion";
+import { motion } from "framer-motion";
 
 //Components
 import BouncingBalls from "./elements/BouncingLogos";
