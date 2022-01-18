ccd0d406d61d14ac059c780d71eff593194d1854
G.M <47867339+GMLJ@users.noreply.github.com>
2022-01-18 18:11:30 +0300

    particles delete

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -2,7 +2,7 @@ import React from "react";
 import WorkCards from "../workCards/workCards";
 
 //Components
-import Particle from "./elements/Particle";
+// import Particle from "./elements/Particle";
 
 const Work = () => {
   return (
@@ -11,7 +11,7 @@ const Work = () => {
         <h2>
           <span>WORK</span>
         </h2>
-        <Particle />
+        {/* <Particle /> */}
       </div>
       <WorkCards />
     </>
